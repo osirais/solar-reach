1c24a314bbaa24ef8a3fe2419b8c5545edfdb35d
Orlando <109116795+osirais@users.noreply.github.com>
2024-12-27 18:56:29 -0500

    three

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,16 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
-import { Cantarell } from "next/font/google";
+import { Cantarell } from 'next/font/google';
 
-const cantarell = Cantarell({ weight: "700" });
+const cantarell = Cantarell({ subsets: ['latin'], weight: '700' });
 
 export const metadata: Metadata = {
   title: "Solar Reach Initiative",
   description:
     "At Solar Reach, we're committed to bringing sustainable, solar-powered light to areas where energy resources are scarce.",
 };
+
 
 export default function RootLayout({
   children,
